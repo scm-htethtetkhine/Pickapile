5e42d340c9b5ac7746043322bf2bd88d8b687fd4
Htet Htet Khine <hhkhine169@gmail.com>
2023-11-01 11:39:22 +0630

    update file(11/1/2023)

---
+++ api/server.js
@@ -0,0 +1,22 @@
+
+const jsonServer = require('json-server/PickAPile.json')
+
+const server = jsonServer.create()
+
+const router = jsonServer.router('json-server/PickAPile.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+})
+
+// Export the Server API
+module.exports = server
